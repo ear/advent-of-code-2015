db9a3954a35cc2e9c1a73fabc346caf8d586a924
Paolo <mrtnpaolo@gmail.com>
2017-12-26 10:15:55 +0100

    🎄 day-05 part 2

--- day-05/Main.hs
+++ day-05/Main.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE LambdaCase #-}
 import Data.List
+import Control.Arrow
 
 main :: IO ()
-main = print =<< length . filter nice . lines <$> readFile "input.txt"
+main = do
+  ws <- lines <$> readFile "input.txt"
+  print . length . filter nice $ ws
+  print . length . filter nice2 $ ws
 
 nice :: String -> Bool
 nice xs = and [blacklist,vowels,twice]
@@ -9,3 +14,13 @@ nice xs = and [blacklist,vowels,twice]
     blacklist = all (`notElem` map (take 2) (tails xs)) ["ab","cd","pq","xy"]
     vowels = (>2) . length . filter (`elem` "aeoiu") $ xs
     twice = any ((>1) . length) . groupBy (==) $ xs
+
+nice2 xs = and [nonoverpair,eye]
+  where
+    nonoverpair
+      = or [ xy `elem` ls || xs `elem` rs
+           | n <- [2 .. length xs - 2]
+           , let (l,(xy,r)) = (id *** splitAt 2) $ splitAt n xs
+           , let ls = map (take 2) (tails l)
+           , let rs = map (take 2) (tails r) ]
+    eye = any (\case [a,_,c] -> a==c; _ -> False) $ map (take 3) (tails xs)
