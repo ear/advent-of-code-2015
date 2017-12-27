3eb3723d66433439f95a0d2cb69eb3d3bde8b394
Paolo <mrtnpaolo@gmail.com>
2017-12-27 14:05:14 +0100

    🎄 day-08 part 2

--- day-08/main.sh
+++ day-08/main.sh
@@ -1,3 +1,5 @@
 total="$(cat input.txt | tr -d '\n' | wc -c)"
 inmem="$(sh unquote.sh | wc -c)"
 echo "${total}-${inmem}" | bc
+total2="$(sh quote.sh | tr -d '\n' | wc -c)"
+echo "${total2}-${total}" | bc
