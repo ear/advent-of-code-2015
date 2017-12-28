2443c99ea3e20b0f19243fbae0a707dbfc65aa5e
Paolo <mrtnpaolo@gmail.com>
2017-12-28 20:28:27 +0100

    🎄 day-10 part 2

--- day-10/Main.hs
+++ day-10/Main.hs
@@ -2,11 +2,11 @@ import Data.List
 import Control.Arrow
 
 main :: IO ()
-main =
-  print
-  . length . head . drop 40 . take 41
-  . iterate (concatMap (\(x,y) -> [x,y])
-             . unfoldr (\xs -> if null xs then Nothing else Just . rle $ xs))
-  $ [1,3,2,1,1,3,1,1,1,2]
+main = print
+     . (length *** length)
+     . ((!! 40) &&& (!! 50))
+     . iterate (concatMap (\(x,y) -> [x,y])
+                . unfoldr (\xs -> if null xs then Nothing else Just . rle $ xs))
+     $ [1,3,2,1,1,3,1,1,1,2]
 
 rle = (first (length &&& head)) . uncurry (span . (==)) . (head &&& id)
