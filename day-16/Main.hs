20e4db6ac7243f804310b3554694e5116a685387
Paolo <mrtnpaolo@gmail.com>
2018-01-01 12:25:51 +0100

    🎄 day-16 part 1

---
+++ day-16/Main.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE TypeApplications #-}
+
+import Text.Parsec
+import Text.Parsec.String
+
+import Data.List
+import Data.Maybe
+import Data.Function
+import Control.Monad
+
+main :: IO ()
+main = do
+  Right r <- parseFromFile p "input.txt"
+  Right t <- parseFromFile t "tape.txt"
+  -- print $ nub . sort . concatMap (map fst . snd) <$> r
+  print . filter (((==) `on` hash) t . snd) $ r
+
+p = do { string "Sue "; n <- read @Int <$> many1 digit; string ": "
+       ; xs <- prop `sepBy` (string ", ")
+       ; return (n,xs) }
+    `sepBy` newline
+
+prop = do { name <- many1 letter
+          ; string ": "
+          ; value <- read @Int <$> many1 digit
+          ; return (name,value) }
+
+t = prop `sepBy` newline
+
+mfcsam = ["akitas","cars","cats","children","goldfish"
+         ,"perfumes","pomeranians","samoyeds","trees","vizslas"]
+
+newtype Hash = H [Maybe Int] deriving Show
+
+instance Eq Hash where
+  (H xs) == (H ys) = null . filter not . catMaybes $ zipWith (liftM2 (==)) xs ys
+
+hash :: [(String,Int)] -> Hash
+hash = H . (`map` mfcsam) . assign
+  where
+    assign ps l = snd <$> find ((l==) . fst) ps
